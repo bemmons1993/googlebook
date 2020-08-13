7183dcb5a7b19ea7049d166e5d465b2d0eef5545
bemmons1993 <61093072+bemmons1993@users.noreply.github.com>
2020-08-12 23:13:06 -0500

    trim

--- server.js
+++ server.js
@@ -5,19 +5,18 @@ const routes= require ("./routes")
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Define middleware here
+// Define middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// Define API routes
 app.use(routes)
 
-//Connet to mongoose
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
 
 
